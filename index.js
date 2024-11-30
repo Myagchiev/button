9f29b8c676486c1387dc41442d104be492e3b4e9
Jambulat Myagchiev <179936292+Myagchiev@users.noreply.github.com>
2024-11-30 23:23:45 +0300

    six

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function myFunction()
 {   
     document.body.style.backgroundColor = "black";
     document.body.style.backgroundImage = "url('ЭКСПОРТ2.png')";
-    document.body.style.backgroundPosition="bottom left";
+    document.body.style.backgroundPosition="bottom center";
     document.body.style.backgroundRepeat = "no-repeat";
 }

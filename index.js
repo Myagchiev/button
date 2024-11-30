2d9361ec51b0ccf5e11caeb169ceb25777130953
Jambulat Myagchiev <179936292+Myagchiev@users.noreply.github.com>
2024-11-30 21:22:47 +0300

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction()
+{   
+    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundImage = "url('ЭКСПОРТ2.png')";
+    document.body.style.backgroundPosition="bottom left";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
